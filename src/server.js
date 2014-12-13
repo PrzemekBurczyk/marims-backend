39083232c857e94f694501991234e5e05d5ac0fa
PrzemekBurczyk <przemek.burczyk@gmail.com>
2014-12-13 20:05:26 +0100

    Improved payloads concatenating.

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+var DEBUG = false;
+
 require('colors');
 var dgram = require('dgram');
 var express = require('express');
@@ -21,7 +23,9 @@ var sessionAndroid = {};
 
 var browsers = io.of(browserEndpoint);
 browsers.on('connection', function(socket){
-  console.log('Browser connected');
+  if(DEBUG){
+    console.log('Browser connected');
+  }
 
   //generate new session id here
   // var sessionId = uuid.v4();
@@ -32,13 +36,17 @@ browsers.on('connection', function(socket){
   var browserUrl = sessionId;
   var browserWebsocketUrl = browserEndpoint + '/' + sessionId;
 
-  //creating browser websocket listener for generated session
+  // creating browser websocket listener for generated session
   sessionBrowsers[sessionId] = io.of(browserWebsocketUrl);
   sessionBrowsers[sessionId].on('connection', function(socket){
-    console.log('Browser connected to session ' + sessionId);
+    if(DEBUG){
+      console.log('Browser connected to session ' + sessionId);
+    }
 
   	socket.on('mouseDown', function(data){
-  	  console.log("mouseDown: x=" + data.xPos + " y=" + data.yPos + " t=" + data.time);
+      if(DEBUG){
+        console.log("mouseDown: x=" + data.xPos + " y=" + data.yPos + " t=" + data.time);
+      }
       androidSocket = sessionAndroid[sessionId];
       if(androidSocket !== undefined && androidSocket !== null){
         androidSocket.emit('motionEvent', {
@@ -50,7 +58,9 @@ browsers.on('connection', function(socket){
       }
   	});
   	socket.on('mouseMove', function(data){
-  	  console.log("mouseMove: x=" + data.xPos + " y=" + data.yPos + " t=" + data.time);
+      if(DEBUG){
+        console.log("mouseMove: x=" + data.xPos + " y=" + data.yPos + " t=" + data.time);
+      }
       androidSocket = sessionAndroid[sessionId];
       if(androidSocket !== undefined && androidSocket !== null){
         androidSocket.emit('motionEvent', {
@@ -62,7 +72,9 @@ browsers.on('connection', function(socket){
       }
   	});
   	socket.on('mouseUp', function(data){
-  	  console.log("mouseUp: x=" + data.xPos + " y=" + data.yPos + " t=" + data.time);
+      if(DEBUG){
+        console.log("mouseUp: x=" + data.xPos + " y=" + data.yPos + " t=" + data.time);
+      }
       androidSocket = sessionAndroid[sessionId];
       if(androidSocket !== undefined && androidSocket !== null){
         androidSocket.emit('motionEvent', {
@@ -75,13 +87,6 @@ browsers.on('connection', function(socket){
   	});
 
   });
-
-  //creating android websocket listener for generated session
-  // sessionAndroid[sessionId] = io.of(androidWebsocketUrl);
-  // sessionAndroid[sessionId].on('connection', function(socket){
-  //   console.log('Android connected to session ' + sessionId);
-  //   socket.emit('start', 'android');
-  // });
 
   //creating browser http listener for generated session
   app.get('/' + sessionId, function(req, res) {
@@ -106,7 +111,7 @@ io.on('connection', function(socket){
   
 });
 
-var port = Number(process.env.PORT || 5000);
+var port = Number(5000);
 http.listen(port, function() {
   console.log(('Listening on ' + port).green);
 });
@@ -147,52 +152,78 @@ udpSocket.on('message', function(msg, rinfo){
   var payloadId = msg[0];
   var payloadCount = msg[1];
   var sessionId = msg.toString('ascii', 3, 39);
-  // console.log('udp message from session: ' + sessionId + ' screenshotId: ' + screenshotId + " -> " + payloadId + '/' + payloadCount + ' from: ' + rinfo.address + ":" + rinfo.port);
-  if(payloadCount > 1){
-    var payload = msg.slice(39);
-    if(buffer[sessionId] === undefined){
-      buffer[sessionId] = { 
-        lastScreenshotId: screenshotId,
-        payloads: new Array(payloadCount)
-      };
-      buffer[sessionId].payloads[payloadId - 1] = payload;
-    } else {
-      var lastScreenshotId = buffer[sessionId].lastScreenshotId;
-      if(screenshotId === lastScreenshotId){
-        buffer[sessionId].payloads[payloadId - 1] = payload;
+  if(sessionBrowsers[sessionId] !== undefined && sessionBrowsers[sessionId] !== null){
+    console.log(screenshotId + ": " + payloadId + "/" + payloadCount);
+    // if whole screen was more than 1 payload
+    if(payloadCount > 1){
+      var payload = msg.slice(39);
+      // if session does not exist in buffer yet
+      if(buffer[sessionId] === undefined){
+        buffer[sessionId] = { 
+          lastSentScreenshotId: null,
+          firstUnsentScreenshotId: null,
+          payloads: {}
+        };
+      } 
+      var lastSentScreenshotId = buffer[sessionId].lastSentScreenshotId;
+
+      // if screenshot that came is after last sent one
+      if(lastSentScreenshotId === null || screenshotId > lastSentScreenshotId || lastSentScreenshotId - screenshotId > 100){
+        // if screenshotId did not come yet
+        if(buffer[sessionId].payloads[screenshotId] === undefined){
+          buffer[sessionId].payloads[screenshotId] = new Array(payloadCount);
+        }
+        // if screenshotId was not flagged as unsent yet
+        if(buffer[sessionId].firstUnsentScreenshotId === null){
+          buffer[sessionId].firstUnsentScreenshotId = screenshotId;
+        }
+        buffer[sessionId].payloads[screenshotId][payloadId - 1] = payload;
+
         var haveCompleteScreenshot = true;
         for(var i = 0; i < payloadCount; i++){
-          if(buffer[sessionId].payloads[i] === undefined){
+          if(buffer[sessionId].payloads[screenshotId][i] === undefined){
             haveCompleteScreenshot = false;
           }
         }
         if(haveCompleteScreenshot === true){
           try {
-          var data = JSON.parse(Buffer.concat(buffer[sessionId].payloads));
+            buffer[sessionId].lastSentScreenshotId = screenshotId;
+            var data = JSON.parse(Buffer.concat(buffer[sessionId].payloads[screenshotId]));
+            sessionBrowsers[sessionId].emit('refresh', { 
+              image: data.image,
+              screenWidth: data.screenWidth,
+              screenHeight: data.screenHeight
+            });
+            var firstUnsentScreenshotId = buffer[sessionId].firstUnsentScreenshotId;
+            var lastSentScreenshotId = buffer[sessionId].lastSentScreenshotId;
+
+            // cleanup old screenshots
+            if(firstUnsentScreenshotId <= lastSentScreenshotId){
+              for(var i = firstUnsentScreenshotId; i <= lastSentScreenshotId; i++){
+                buffer[sessionId].payloads[i] = undefined;
+              }
+            } else {
+              for(var i = firstUnsentScreenshotId; i < 256; i++){
+                buffer[sessionId].payloads[i] = undefined;
+              }
+              for(var i = 0; i <= lastSentScreenshotId; i++){
+                buffer[sessionId].payloads[i] = undefined;
+              }
+            }
+            buffer[sessionId].firstUnsentScreenshotId = (lastSentScreenshotId + 1) % 256;
           } catch(err) {
             console.log(err);
           }
-          sessionBrowsers[sessionId].emit('refresh', { 
-            image: data.image,
-            screenWidth: data.screenWidth,
-            screenHeight: data.screenHeight
-          });
         }
-      } else if(screenshotId > lastScreenshotId || lastScreenshotId - screenshotId > 100){
-        buffer[sessionId] = { 
-          lastScreenshotId: screenshotId,
-          payloads: new Array(payloadCount)
-        };
-        buffer[sessionId].payloads[payloadId - 1] = payload;
       }
+    } else if(payloadCount === 1) { // if screen was only 1 payload
+      var data = JSON.parse(msg.slice(39));
+      sessionBrowsers[sessionId].emit('refresh', { 
+        image: data.image,
+        screenWidth: data.screenWidth,
+        screenHeight: data.screenHeight
+      });
     }
-  } else if(payloadCount === 1) {
-    var data = JSON.parse(msg.slice(39));
-    sessionBrowsers[sessionId].emit('refresh', { 
-      image: data.image,
-      screenWidth: data.screenWidth,
-      screenHeight: data.screenHeight
-    });
   }
 });
 
