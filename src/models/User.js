bd24ba3cb7132657eefc2316bc751cd85a5fc322
Przemek Burczyk <przemek.burczyk@gmail.com>
2016-02-21 21:46:53 +0100

    Changed model to accept file authors/membership. Implemented methods to add/remove author/member

--- src/models/User.js
+++ src/models/User.js
@@ -10,7 +10,9 @@ var UserSchema = new Schema({
         type: String,
         unique: true,
         required: true
-    }
+    },
+    authorOfFiles: [String],
+    memberOfFiles: [String]
 });
 
 UserSchema.index({
@@ -30,4 +32,41 @@ module.exports.create = function(user, password, callback) {
 module.exports.getByEmail = function(email, callback) {
     if (_.isNull(email) || _.isUndefined(email)) return callback();
     User.findOne({ email: email }).exec(callback);
+};
+
+module.exports.addFileAuthor = function(id, filename, callback) {
+    if (_.isNull(id) || _.isUndefined(id)) return callback();
+    if (_.isNull(filename) || _.isUndefined(filename)) return callback();
+
+    User.findByIdAndUpdate(id, {
+        $addToSet: {
+            authorOfFiles: filename
+        }
+    }).exec(callback);
+};
+
+module.exports.addFileMember = function(email, filename, callback) {
+    if (_.isNull(email) || _.isUndefined(email)) return callback();
+    if (_.isNull(filename) || _.isUndefined(filename)) return callback();
+
+    User.findOneAndUpdate({
+        email: email
+    }, {
+        $addToSet: {
+            memberOfFiles: filename
+        }
+    }).exec(callback);
+};
+
+module.exports.removeFileMember = function(email, filename, callback) {
+    if (_.isNull(email) || _.isUndefined(email)) return callback();
+    if (_.isNull(filename) || _.isUndefined(filename)) return callback();
+
+    User.findOneAndUpdate({
+        email: email
+    }, {
+        $pull: {
+            memberOfFiles: filename
+        }
+    }).exec(callback);
 };
